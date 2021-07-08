843c00d9a98ded0a75c106ec3fadbdf576e28fed
Chase Wallace <chase.wallace@gmail.com>
2021-07-08 03:20:52 -0500

    set of changes throughout for readability, styling, etc

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 An Online Magic 8 Ball
 ============
-WYSIWYG. Pretty basic web app that utilizes Firebase to get canned responses for the 8 ball and push questions asked.
+WYSIWYG. Pretty basic web app that utilizes Firebase to get canned responses for the 8 ball while saving questions asked.
+
+*Skills Used*
+Generate Unique Randomized User ID
+Get from and Push to Remote Database
+Define Specific Rules for Allowing/Denying Remote Database Changes
+DOM Manipulation
+Inclusion and Utilization of JS Libraries
+Basic Sanitization of Form Inputs
+CSS Animations + Flexbox
--- main.js
+++ main.js
@@ -12,11 +12,15 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
+//some global variables
 var database = firebase.database(),
     ansLen,
     tempQuest,
     answers,
+    userId,
     going = false;
+
+//get the responses for later and set some defaults if something goes wonky
 database.ref().child("responses").get().then((snapshot) => {
     if (snapshot.exists()) {
         ansLen = snapshot.val().length;
@@ -29,18 +33,33 @@ database.ref().child("responses").get().then((snapshot) => {
     console.error(error);
 });
 
-//basic sanitation for strings
+//basic sanitation function for strings
 function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
-//align the splash around the ball
+//function to create a unique question ID
+function newQID() {
+    var now = Date.now(),
+        tempId = Math.floor((now * Math.random()) * 0x10000).toString(16);
+    database.ref('answered/'+tempId).get().then((snapshot) => {
+        if (snapshot.exists()) {
+            newQID();
+        } else {
+            userId = tempId;
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+};
+
+//align the splash title around the ball
 function radialLetters() {
     var letterCount = document.getElementsByTagName('span').length,
         spacing = 4,
         smallLetters = 1,
         maxLetter = Math.ceil(letterCount/2);
-    console.log(letterCount);
+    
     //this block checks for question length
     //and sets variables appropriately
     if (letterCount>90 && letterCount<181) {
@@ -55,7 +74,8 @@ function radialLetters() {
         $("h3").text("That was a really long question, dude. Hope you remember it.");
         return;
     }
-    //loop through them individual letters! do it!
+    
+    //loop through the individual letters! do it now!
     for (i=0; i<letterCount; i++) {
         var currentCount = i+1,
             degreeTurn = (currentCount - maxLetter)*spacing,
@@ -77,36 +97,47 @@ function radialLetters() {
 $(document).ready(function(){
     $("#splash").lettering();
     
+    //initialize the splash title after the page has had a chance to breathe
     setTimeout(function() {
         $("#splash").fadeIn(1000);
         radialLetters();
     },10);
+
+    //bing bang boom, off we go!
     $("form").on("submit", function(e){
         e.preventDefault();
         if (!going) watchIt();
     });
 });
 
+//some global functions
+
 //watch for those buttons!
 var watchIt = function() {
+
+    //change flag to prevent double sending while this is all going on
     going = true;
+
+    //get value of the question
     var inputz = $("#quest").val();
         if (inputz === "") {
             tryAgain();
         }
         else if (inputz !== "") {
+            newQID();
             doItAll(inputz);
         }
     },
+
     //this makes them try again if nothing entered into field
     tryAgain = function() {
-        $("#splash").text("Try Asking Me A Question This Time!");
+        $("#splash").text("Try Asking A Question This Time!");
         $("#splash").lettering();
         radialLetters();
     },
-    //this does it all when enter or button is clicked
+
+    //this does it all when enter or button is clicked with a question
     doItAll = function(dataz) {
-        questionCounter++;
         $("#splash").text(dataz);
         $("#splash").lettering();
         shakeThemUp();
@@ -116,49 +147,36 @@ var watchIt = function() {
         $("#quest").val("");
     },
 
-    //this toggles some shake magic!
+    //function to show some shake magic!
     shakeThemUp = function() {
-        console.log("shaking");
         $("#container").toggleClass("shakeIt");
         $("#fate").toggleClass("inverseShakeIt");
     },
 
-    //this one is for fading out the previous response and starting the choice
+    //function for fading out the previous response
     goingOut = function() {
         $("#fateText").text("");
         $("#fate").fadeOut(1000, "linear", setTimeout(postIt, 2000));
     },
 
-    //this one creates a magic number
+    //function to create a magic number
     randoms = function(){
         var numbre = Math.floor(Math.random()*ansLen);
         return numbre;
     },
 
-    //so this guy is going to generate a random number and pick a response
-    //this guy is going to fade the response in and turn the shakes off
-    //this collects the generously donated
-    //questions for the class survey
+    //shake it, grab an answer, and populate the fated choice and database
     postIt = function() {
         shakeThemUp();
-        var now = Date.now(),
-            answer = answers[randoms()];
-            userId = Math.floor((now * Math.random()) * 0x10000).toString(16);
+        var answer = answers[randoms()];
         setTimeout(function() {
             $("#fate").fadeIn(2000);
             $("#fateText").fadeIn(2000);
             $("#fateText").text(answer);
         }, 10)
-        firebase.database().ref('answered/'+userId).set({
+        database.ref('answered/'+userId).set({
             answer: answer,
             question : tempQuest
         });
         going = false;
-    },
-
-    //variable declarations, hooooo!
-    inputz,
-    questionCounter = 0;
-
-
-
+    };
