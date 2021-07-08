fab526b0d0477584d8b7d9f9675be8552d2b1d1a
Chase Wallace <cwallace@archermalmo.com>
2021-07-08 01:57:29 -0500

    just need to get shaking back

--- main.js
+++ main.js
@@ -1,14 +1,95 @@
+//initialize the firebase stuff
+var firebaseConfig = {
+    apiKey: "AIzaSyAmgUk0ypJzhXsZ5oVBkKOxF3TW9fNBSVw",
+    authDomain: "magic-8-a5c33.firebaseapp.com",
+    projectId: "magic-8-a5c33",
+    databaseURL: "https://magic-8-a5c33-default-rtdb.firebaseio.com",
+    storageBucket: "magic-8-a5c33.appspot.com",
+    messagingSenderId: "796689420968",
+    appId: "1:796689420968:web:36adf4669fb45d85f6289d",
+    measurementId: "G-4Z3TXQCN29"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var database = firebase.database(),
+    ansLen,
+    tempQuest,
+    answers,
+    going = false;
+database.ref().child("responses").get().then((snapshot) => {
+    if (snapshot.exists()) {
+        ansLen = snapshot.val().length;
+        answers = snapshot.val();
+    } else {
+        ansLen = 2;
+        return ["ehh", "nope"]
+    }
+}).catch((error) => {
+    console.error(error);
+});
+
+//basic sanitation for strings
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+//align the splash around the ball
+function radialLetters() {
+    var letterCount = document.getElementsByTagName('span').length,
+        spacing = 4,
+        smallLetters = 1,
+        maxLetter = Math.ceil(letterCount/2);
+    console.log(letterCount);
+    //this block checks for question length
+    //and sets variables appropriately
+    if (letterCount>90 && letterCount<181) {
+        smallLetters = 2;
+        spacing = 2;
+    }
+    else if (letterCount>180 && letterCount<361) {
+        smallLetters = 3;
+        spacing = 1;
+    }
+    else if (letterCount>360) {
+        $("h3").text("That was a really long question, dude. Hope you remember it.");
+        return;
+    }
+    //loop through them individual letters! do it!
+    for (i=0; i<letterCount; i++) {
+        var currentCount = i+1,
+            degreeTurn = (currentCount - maxLetter)*spacing,
+            currentChar = "char"+currentCount,
+            stylingBlock = "-ms-transform: rotate("+degreeTurn+"deg); -webkit-transform: rotate("+degreeTurn+"deg); transform: rotate("+degreeTurn+"deg);";
+        if (smallLetters === 2){
+            $("."+currentChar).attr("style", stylingBlock+" font-size: 18px");
+        }
+        else if (smallLetters === 3) {
+            $("."+currentChar).attr("style", stylingBlock+" font-size: 12px");
+        }
+        else {
+            $("."+currentChar).attr("style", stylingBlock);
+        }
+    }
+}
+
 //lets kick this all off!
 $(document).ready(function(){
-    letterizeIt(null);
+    $("#splash").lettering();
+    
+    setTimeout(function() {
+        $("#splash").fadeIn(1000);
+        radialLetters();
+    },10);
     $("form").on("submit", function(e){
         e.preventDefault();
-        watchIt();
+        if (!going) watchIt();
     });
 });
 
 //watch for those buttons!
 var watchIt = function() {
+    going = true;
     var inputz = $("#quest").val();
         if (inputz === "") {
             tryAgain();
@@ -17,129 +98,67 @@ var watchIt = function() {
             doItAll(inputz);
         }
     },
-//this makes them try again if
-//nothing entered into field
+    //this makes them try again if nothing entered into field
     tryAgain = function() {
         $("#splash").text("Try Asking Me A Question This Time!");
-        letterizeIt(null);
+        $("#splash").lettering();
+        radialLetters();
     },
-//this does it all when enter
-//or button is clicked
+    //this does it all when enter or button is clicked
     doItAll = function(dataz) {
         questionCounter++;
-        shakeThemUp();
-        goingOut();
         $("#splash").text(dataz);
-        letterizeIt(dataz);
+        $("#splash").lettering();
+        shakeThemUp();
+        radialLetters();
+        tempQuest = escapeRegExp(dataz);
         $("#quest").val("");
+        postIt();
     },
 
-//this is where the circular question
-//magic happens
-    letterizeIt = function(question) {
-        $("h3").lettering();
-        var spacing = 4,
-        smallLetters = 1;
-        if (question !== null) {
-            postIt(question);
-        }
-        var letterCount = document.getElementsByTagName('span').length,
-            maxLetter = Math.ceil(letterCount/2);
-
-//this block checks for question length
-//and sets variables appropriately
-        if (letterCount>90 && letterCount<181) {
-            smallLetters = 2;
-            spacing = 2;
-        }
-        else if (letterCount>180 && letterCount<361) {
-            smallLetters = 3;
-            spacing = 1;
-        }
-        else if (letterCount>360) {
-            $("h3").text("Nope, That's Too Many Characters For You.");
-            letterizeIt(null);
-            return;
-        }
-
-//loop through them individual letters!
-//do it!
-        for (i=0; i<letterCount; i++) {
-            var currentCount = i+1,
-                degreeTurn = (currentCount - maxLetter)*spacing,
-                currentChar = "char"+currentCount,
-                stylingBlock = "-ms-transform: rotate("+degreeTurn+"deg); -webkit-transform: rotate("+degreeTurn+"deg); transform: rotate("+degreeTurn+"deg);";
-            if (smallLetters === 2){
-                $("."+currentChar).attr("style", stylingBlock+" font-size: 18px");
-            }
-            else if (smallLetters === 3) {
-                $("."+currentChar).attr("style", stylingBlock+" font-size: 12px");
-            }
-            else {
-                $("."+currentChar).attr("style", stylingBlock);
-            }
-        }
-    },
-
-//this toggles some shake magic!
+    //this toggles some shake magic!
     shakeThemUp = function() {
+        console.log("shaking");
         $("#container").toggleClass("shakeIt");
         $("#fate").toggleClass("inverseShakeIt");
     },
 
-//this one is for fading out the previous
-//response and starting the choice
+    //this one is for fading out the previous response and starting the choice
     goingOut = function() {
-        $("#fate").fadeOut(1000, "linear", setTimeout(chooseOne, 2000));
-    },
-
-//so this guy is going to generate
-//a random number and pick a response
-    chooseOne = function() {
-        var wantSome = randoms();
         $("#fateText").text("");
-        waitingTime(wantSome);
+        $("#fate").fadeOut(1000, "linear", setTimeout(postIt, 2000));
     },
 
-//this guy is going to fade the response
-//in and turn the shakes off
-    waitingTime = function(digits) {
-        var chileConQueso = choiced(digits);
-        grabbingOn(chileConQueso);
-        shakeThemUp();
-        $("#fate").fadeIn(2000);
-    },
-
-//this one creates a magic number
+    //this one creates a magic number
     randoms = function(){
-        var numbre = Math.floor(Math.random()*14)+1;
+        var numbre = Math.floor(Math.random()*ansLen);
         return numbre;
     },
 
-//this guy creates the choice id
-    choiced = function(numero) {
-        var bango = "choice"+numero;
-        return bango;
-    },
-
-//this collects the generously donated
-//questions for the class survey
-    postIt = function (question){
-        var postData = new Firebase('https://amber-fire-3354.firebaseio.com/surveyData/'+generateGUID+"/"+questionCounter);
-        postData.set(question);
-    },
-
-//this guy makes the proper call
-//to the proper id
-    grabbingOn = function(whatWhat){
-        fetchData = new Firebase('https://amber-fire-3354.firebaseio.com/'+whatWhat);
-        fetchData.once("value", function(whatWhat) {
-            var quesoDip = whatWhat.val();
-            $("#fateText").text(quesoDip);
+    //so this guy is going to generate a random number and pick a response
+    //this guy is going to fade the response in and turn the shakes off
+    //this collects the generously donated
+    //questions for the class survey
+    postIt = function() {
+        shakeThemUp();
+        var now = Date.now(),
+            answer = answers[randoms()];
+            userId = Math.floor((now * Math.random()) * 0x10000).toString(16);
+        setTimeout(function() {
+            $("#fate").fadeIn(2000);
+            $("#fateText").fadeIn(2000);
+            $("#fateText").text(answer);
+        }, 2000)
+        firebase.database().ref('answered/'+userId).set({
+            answer: answer,
+            question : tempQuest
         });
+        going = false;
     },
 
-//variable declarations, hooooo!
+    //variable declarations, hooooo!
     inputz,
-    questionCounter = 0,
-    generateGUID = Math.floor((100000000000 + Math.random()) * 0x10000).toString(16);
+    questionCounter = 0;
+
+
+
